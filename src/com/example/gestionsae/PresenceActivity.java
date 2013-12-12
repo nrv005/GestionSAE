a9220f08b239942832befd6cd22adecbf895d608
nrv005 <nrv@linux-alpes.org>
2013-12-12 18:21:28 +0100

    Affiche l'activitée "Presence".

--- src/com/example/gestionsae/PresenceActivity.java
+++ src/com/example/gestionsae/PresenceActivity.java
@@ -1,5 +1,43 @@
 package com.example.gestionsae;
 
-public class PresenceActivity {
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class PresenceActivity extends Activity{
+	
+	GestionBdd saedb = new GestionBdd(this);
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_pesence);
+		
+		
+		/**
+		 * Button.setOnClickListener : Gestion du bouton "Nouveau Membre"
+		 * On clique sur le bouton pour afficher une boite de dialogue "R.layout.new_membre"
+		 * On obtient le nom et prénom, que l'on rajoute dans la table membre nom,prenom
+		 */
+		Button newMembre = (Button) findViewById(R.id.btPresenceNewMembre);
+		newMembre.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				
+				LayoutInflater inflater = getLayoutInflater();
+				View dialogLayout = inflater.inflate(R.layout.new_membre, null);
+				AlertDialog.Builder builder = new AlertDialog.Builder(PresenceActivity.this);
+				builder.setView(dialogLayout);
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
 
 }
